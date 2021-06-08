d752188e98ae2d65762aeeb50371825baeb977b2
MatthewMunks <matthewmunks@gmail.com>
2021-06-08 14:00:24 +0100

    Getting rid of the always statements within Ex4/top_tb.v

--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -61,73 +61,140 @@ module top_tb();
             $display("***TEST FAILED! Colour should be changing!***");   
         end
     end
-    
-    always @(posedge clk) begin
-        if (colour == 'b111 || colour == 'b000) begin 
-            err = 1;
-            $display("***TEST FAILED! That is an illegal state!***");        
-        end       
-
-        if (colour == STOP_AT) begin
-            button = 0;
-            #70
-            if (colour != STOP_AT) begin
+    initial begin
+        #(CLK_PERIOD/2)
+        forever begin
+            #CLK_PERIOD
+            if (colour == 'b111 || colour == 'b000) begin 
                 err = 1;
-                $display("***TEST FAILED! When the button is not being pressed, we don't want the lights to be incrementing!***");
-            end
-            button = 1;
-            #70
+                $display("***TEST FAILED! That is an illegal state!***");        
+            end   
+
             if (colour == STOP_AT) begin
-                err = 1;
-                $display("***TEST FAILED! When the button is being pressed, we want the lights to be incrementing!***");
-            end
-            
-            if (testPhase == STOP_AT_4) begin
-                STOP_AT = 3;
-                testPhase = STOP_AT_3;
-            end else if (testPhase == STOP_AT_3) begin
-                STOP_AT = 0;    //Illegal state and so should never be triggered!
-                testPhase = DONT_STOP;
-                
-                #100 //Testing Reset again
-                rst = 1;
-                #5
-                if (colour != 1) begin
-                    err = 1;
-                    $display("***TEST FAILED! Reset should reset the value***");
-                end
+                button = 0;
                 #70
-                if (colour != 1) begin
+                if (colour != STOP_AT) begin
                     err = 1;
-                    $display("***TEST FAILED! Reset should reset the value***");
+                    $display("***TEST FAILED! When the button is not being pressed, we don't want the lights to be incrementing!***");
                 end
-                rst = 0;
-                #30
-                if (colour == 1) begin
+                button = 1;
+                #70
+                if (colour == STOP_AT) begin
                     err = 1;
-                    $display("***TEST FAILED! Reset is now off and so colour should be increasing again***");
+                    $display("***TEST FAILED! When the button is being pressed, we want the lights to be incrementing!***");
                 end
                 
-            end
+                if (testPhase == STOP_AT_4) begin
+                    STOP_AT = 3;
+                    testPhase = STOP_AT_3;
+                end else if (testPhase == STOP_AT_3) begin
+                    STOP_AT = 0;    //Illegal state and so should never be triggered!
+                    testPhase = DONT_STOP;
+                    
+                    #100 //Testing Reset again
+                    rst = 1;
+                    #5
+                    if (colour != 1) begin
+                        err = 1;
+                        $display("***TEST FAILED! Reset should reset the value***");
+                    end
+                    #70
+                    if (colour != 1) begin
+                        err = 1;
+                        $display("***TEST FAILED! Reset should reset the value***");
+                    end
+                    rst = 0;
+                    #30
+                    if (colour == 1) begin
+                        err = 1;
+                        $display("***TEST FAILED! Reset is now off and so colour should be increasing again***");
+                    end
+                    
+                end
             
-
-        end
-
-    end    
-
-    //Not a race condition because we're not updating the same variable (bar err and that only gets set to 1, and so doesn't create a race condition)
-    always @(posedge clk) begin
-        //Testing incrementation of colour
-        //Once we're in the DONT_STOP phase, this section fails when the reset comes off because both colour and colourPrevState are in state 1. 
-        //We've already tested the incrementation post rst=0 so we don't really need to test it again.
-        if (button == 1 && rst == 0 && testPhase != DONT_STOP) begin
-            if (~(colour == colourPrevState + 1 || (colour == 1 && colourPrevState == 6))) begin 
-                err = 1;
-                $display("***TEST FAILED! Colour is not incremenmting***"); 
             end
-            colourPrevState = colour;
         end
     end
+    
+    // always @(posedge clk) begin
+    //     if (colour == 'b111 || colour == 'b000) begin 
+    //         err = 1;
+    //         $display("***TEST FAILED! That is an illegal state!***");        
+    //     end       
+
+    //     if (colour == STOP_AT) begin
+    //         button = 0;
+    //         #70
+    //         if (colour != STOP_AT) begin
+    //             err = 1;
+    //             $display("***TEST FAILED! When the button is not being pressed, we don't want the lights to be incrementing!***");
+    //         end
+    //         button = 1;
+    //         #70
+    //         if (colour == STOP_AT) begin
+    //             err = 1;
+    //             $display("***TEST FAILED! When the button is being pressed, we want the lights to be incrementing!***");
+    //         end
+            
+    //         if (testPhase == STOP_AT_4) begin
+    //             STOP_AT = 3;
+    //             testPhase = STOP_AT_3;
+    //         end else if (testPhase == STOP_AT_3) begin
+    //             STOP_AT = 0;    //Illegal state and so should never be triggered!
+    //             testPhase = DONT_STOP;
+                
+    //             #100 //Testing Reset again
+    //             rst = 1;
+    //             #5
+    //             if (colour != 1) begin
+    //                 err = 1;
+    //                 $display("***TEST FAILED! Reset should reset the value***");
+    //             end
+    //             #70
+    //             if (colour != 1) begin
+    //                 err = 1;
+    //                 $display("***TEST FAILED! Reset should reset the value***");
+    //             end
+    //             rst = 0;
+    //             #30
+    //             if (colour == 1) begin
+    //                 err = 1;
+    //                 $display("***TEST FAILED! Reset is now off and so colour should be increasing again***");
+    //             end
+                
+    //         end
+            
+
+    //     end
+
+    // end    
+
+    initial begin
+        #(CLK_PERIOD/2)
+        forever begin
+            if (button == 1 && rst == 0 && testPhase != DONT_STOP) begin
+                if (~(colour == colourPrevState + 1 || (colour == 1 && colourPrevState == 6))) begin 
+                    err = 1;
+                    $display("***TEST FAILED! Colour is not incremenmting***"); 
+                end
+                colourPrevState = colour;
+            end
+        end
+    end
+
+    //Not a race condition because we're not updating the same variable (bar err and that only gets set to 1, and so doesn't create a race condition)
+    // always @(posedge clk) begin
+    //     //Testing incrementation of colour
+    //     //Once we're in the DONT_STOP phase, this section fails when the reset comes off because both colour and colourPrevState are in state 1. 
+    //     //We've already tested the incrementation post rst=0 so we don't really need to test it again.
+    //     if (button == 1 && rst == 0 && testPhase != DONT_STOP) begin
+    //         if (~(colour == colourPrevState + 1 || (colour == 1 && colourPrevState == 6))) begin 
+    //             err = 1;
+    //             $display("***TEST FAILED! Colour is not incremenmting***"); 
+    //         end
+    //         colourPrevState = colour;
+    //     end
+    // end
 
     //Finish test, check for success
     initial begin
