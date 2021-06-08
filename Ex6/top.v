9b0fdf2236f97f979f75dc4f62c11193488440a0
MatthewMunks <matthewmunks@gmail.com>
2021-06-08 15:47:40 +0100

    Beginning work on Ex6/top.v

--- Ex6/top.v
+++ Ex6/top.v
@@ -15,3 +15,13 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 
+ColourLookUp your_instance_name (
+  .clka(clka),    // input wire clka
+  .ena(ena),      // input wire ena
+  .wea(wea),      // input wire [0 : 0] wea
+  .addra(addra),  // input wire [3 : 0] addra
+  .dina(dina),    // input wire [15 : 0] dina
+  .douta(douta)  // output wire [15 : 0] douta
+);
+
+module colourConverter
