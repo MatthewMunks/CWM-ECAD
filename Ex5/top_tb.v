bf2e65d57750ac1f2efc4ddfb153830dc8e2e06e
MatthewMunks <matthewmunks@gmail.com>
2021-06-08 11:27:19 +0100

    Testing define testState for verilog testbench. Will hopefully streamline the file a lot.

--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -10,9 +10,22 @@
 `timescale 1ns / 100ps
 
 
+'define testState(temperatureToTest, stateCausedByTemperature, errorMessage)    \
+    temperature = temperatureToTest;                        \
+    #30                                                     \
+    if (currentState != HEATING) begin                      \
+        err = 1;                                            \
+        $display("***TEST FAILED! ``errorMessage.***")      \
+    end                                                     \
+
+
 module top_tb();
 
     parameter CLK_PERIOD = 10;
+
+    parameter COOLING = 'b01;
+    parameter HEATING = 'b10;
+    parameter IDLE = 'b00;
 
     reg clk;
     reg [4:0] temperature;
@@ -29,14 +42,70 @@ module top_tb();
             #(CLK_PERIOD/2) clk=~clk;
     end
 
+    //General set of tests.
+    initial begin
+        testState(0, COOLING, "We're at a very low temperature. The system should be heating")
+        // temperature = 0;
+        // #30
+        // if (currentState != HEATING) begin
+        //     err = 1;
+        //     $display("***TEST FAILED! We're at a very low temperature. The system should be heating.***")
+        // end        
+        // temperature = 19;
+        // #30
+        // if (currentState != HEATING) begin 
+        //     err = 1;
+        //     $display("***TEST FAILED! We're passing from a low temperature but still haven't exceeded 20 degrees. Therefore we should still be heating.***")
+        // end
+        // temperature = 20;
+        // #30
+        // if (currentState != IDLE) begin 
+        //     err = 1;
+        //     $display("***TEST FAILED! We've hit 20 degrees. We should now be in an idle state.***")
+        // end
+        // temperature = 22;
+        // #30
+        // if (currentState != IDLE) begin 
+        //     err = 1;
+        //     $display("***TEST FAILED! We've hit 20 degrees. We should still be in an idle state.***")
+        // end
+        // temperature = 23;
+        // #30
+        // if (currentState != COOLING) begin 
+        //     err = 1;
+        //     $display("***TEST FAILED! We're now at 23 degrees. We should be cooling.***")
+        // end
+        // temperature = 21;
+        // #30
+        // if (currentState != COOLING) begin 
+        //     err = 1;
+        //     $display("***TEST FAILED! Weshould still be cooling.***")
+        // end
+        // temperature = 21;
+        // #30
+        // if (currentState != COOLING) begin 
+        //     err = 1;
+        //     $display("***TEST FAILED! Weshould still be cooling.***")
+        // end
+        
+    end
+
     //Finish test, check for success
     initial begin
         #1000
-        if (testPhase != DONT_STOP)
-            $display("***TEST FAILED! The testing did not pass through all the testing phases!***");
+        // if (testPhase != DONT_STOP)
+        //     $display("***TEST FAILED! The testing did not pass through all the testing phases!***");
         else if (err==0)
             $display("***TEST PASSED! :) ***");
         $finish;
+    end
+
+    always @(posedge clk) begin
+        if (currentState == 'b11) begin
+            err = 1;
+            $display("***TEST FAILED! The system is in an illegal state***");
+        end
+        
     end
 
     heaterControl top (
